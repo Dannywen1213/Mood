6d7b1c289ad2c8d42646af6e98c56484d7e51a73
Yifan <wendanny@grinnll.edu>
2024-12-09 21:02:38 -0600

    Initial Commit

---
+++ Mood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mood
+//
+//  Created by Yifan Wen on 2024/12/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoodTests/MoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTests.swift
+//  MoodTests
+//
+//  Created by Yifan Wen on 2024/12/9.
+//
+
+import Testing
+@testable import Mood
+
+struct MoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
